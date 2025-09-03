46842e39d0432534914feafc3a2003ac2188a930
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-03 07:13:19 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic import DetailView
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book, Library
-
+from django.contrib.auth.decorators import user_passes_test
 
 # ---------------------------
 # Function-Based View (Books)
@@ -59,3 +59,28 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")
+
+# Helper functions
+def is_admin(user):
+    return hasattr(user, "profile") and user.profile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "profile") and user.profile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "profile") and user.profile.role == "Member"
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
