5b39b73f1e39d29ff2d66ab96a7a9b5593c5cf28
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-27 14:53:44 +0100

    Create settings.py

---
+++ advanced-api-project/settings.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Third-party apps
+    "rest_framework",
+
+    # Local apps
+    "api",
+]
