9d9ae66a24d44c2276b7ab833d4aea7acbdbdd80
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-27 15:36:20 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 # ✅ List all books (public read-only access)
 class BookListView(generics.ListAPIView):
