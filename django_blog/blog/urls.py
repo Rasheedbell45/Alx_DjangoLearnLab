876046706f69eeb44883795eaa502048cca38dbc
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-03 14:48:33 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import (
-    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-)
+from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
@@ -11,6 +10,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # updated URL
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
