db1715d804fe7143c37cd4b092bff91bf005f962
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-03 15:53:31 +0100

    Update urls.py

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # Include blog URLs
+    path('', include('blog.urls')),
 ]
