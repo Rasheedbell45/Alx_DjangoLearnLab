c5d44445538cdf9f99dad67a415f271f598ecfde
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-03 13:36:08 +0100

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -1,3 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
