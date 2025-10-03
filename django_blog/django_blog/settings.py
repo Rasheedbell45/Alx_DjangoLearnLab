52b92c68975136c807d9e123d684ad23bd3ab286
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-03 13:31:00 +0100

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -5,5 +5,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',  # new app
+    'blog.apps.BlogConfig',
+    'blog'
 ]
