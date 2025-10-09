f68a8636bf37d817547d0e03c59e01e024384f07
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-09 14:31:21 +0100

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -13,6 +13,8 @@ INSTALLED_APPS = [
 
     # your apps
     "accounts",
+    "posts",
+    "django_filters",
 ]
 
 # custom user model
@@ -20,10 +22,14 @@ AUTH_USER_MODEL = "accounts.User"
 
 # rest framework settings (simple example)
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-        # optionally add SessionAuthentication during dev:
         "rest_framework.authentication.SessionAuthentication",
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
