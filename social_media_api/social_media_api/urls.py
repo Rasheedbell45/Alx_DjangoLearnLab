8000424a3a2d6bdd95526ae9fa699bf123e9bf19
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-09 14:40:11 +0100

    Create urls.py

---
+++ social_media_api/social_media_api/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken import views as drf_views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/auth/", drf_views.obtain_auth_token, name="api-token-auth"),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/posts/", include("posts.urls")),
+]
