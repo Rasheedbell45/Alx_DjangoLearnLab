cbca6944c2b0c576e3719402c41bf19c1b53339e
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-09 14:47:54 +0100

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView, ProfileAPIView, PublicProfileAPIView
+from .views import RegisterAPIView, LoginAPIView, ProfileAPIView, PublicProfileAPIView, FollowUserAPIView, UnfollowUserAPIView, FollowersListAPIView, FollowingListAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
     path("login/", LoginAPIView.as_view(), name="login"),
     path("profile/", ProfileAPIView.as_view(), name="profile"),
     path("users/<str:username>/", PublicProfileAPIView.as_view(), name="public-profile"),
+    path("follow/<int:user_id>/", FollowUserAPIView.as_view(), name="follow-user"),
+    path("unfollow/<int:user_id>/", UnfollowUserAPIView.as_view(), name="unfollow-user"),
+    path("users/<int:user_id>/followers/", FollowersListAPIView.as_view(), name="user-followers"),
+    path("users/<int:user_id>/following/", FollowingListAPIView.as_view(), name="user-following"),
 ]
