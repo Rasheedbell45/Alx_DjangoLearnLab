d47a24ce5810f335252faaea69fd7064ad390004
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-09 14:37:24 +0100

    Update urls.py

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),  # all accounts endpoints under /api/accounts/
+   path("admin/", admin.site.urls),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/", include("posts.urls")),
 ]
 
 if settings.DEBUG:
